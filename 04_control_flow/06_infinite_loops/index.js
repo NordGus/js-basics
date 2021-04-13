8301032a6fd3aedde743bde81512c906d33254cc
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2021-04-13 17:30:37 +0200

    examples of infinite loops

--- 04_control_flow/06_infinite_loops/index.js
+++ 04_control_flow/06_infinite_loops/index.js
@@ -1,1 +1,18 @@
-// If...else
+// Infinite Loops
+
+let i = 0;
+
+while (i < 5) {
+    console.log(i);
+    //i++;
+}
+
+while (true) {}
+
+do {
+    // i++
+} while (i < 5);
+
+for (let i = 0; i > 0; i++) {}
+
+for (let i = 0; i > 0;) {}
