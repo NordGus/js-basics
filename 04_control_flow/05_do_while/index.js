a63e932f293813dad443b5bb5aed7f71894b8f5d
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2021-04-13 17:23:49 +0200

    simple do...while loop

--- 04_control_flow/05_do_while/index.js
+++ 04_control_flow/05_do_while/index.js
@@ -1,1 +1,13 @@
-// If...else
+// Do...While
+// Do...While loops are executed at leat onces
+
+function loop(i) {
+    do {
+        if (i % 2 !== 0) console.log(i);
+        i++;
+    } while (i <= 5);
+    
+}
+
+loop(0);
+loop(9);
