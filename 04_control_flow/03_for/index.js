a2324d9e5f09ec280647a3edbcf672609167e8fd
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2021-04-13 17:12:51 +0200

    basic for statement

--- 04_control_flow/03_for/index.js
+++ 04_control_flow/03_for/index.js
@@ -1,1 +1,11 @@
-// If...else
+// For
+
+for (let i = 1; i <= 5; i++) {
+    if (i % 2 !== 0)
+        console.log(i);
+}
+
+for (let i = 5; i >= 1; i--) {
+    if (i % 2 !== 0)
+        console.log(i);
+}
