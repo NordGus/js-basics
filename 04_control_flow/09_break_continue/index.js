8db041ba5ab77efd864efb77fddc2d676e70ae1e
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2021-04-14 19:27:35 +0200

    break and continue keywords

--- 04_control_flow/09_break_continue/index.js
+++ 04_control_flow/09_break_continue/index.js
@@ -1,1 +1,24 @@
-// If...else
+// Break and Continue
+
+let i = 0;
+
+console.log('break');
+while (i <= 10) {
+    if (i == 5) break;
+
+    console.log(i);
+    i++;
+}
+
+i = 0;
+
+console.log('continue');
+while (i <= 10) {
+    if (i % 2 === 5) {
+        i++;
+        continue;
+    }
+
+    console.log(i);
+    i++;
+}
