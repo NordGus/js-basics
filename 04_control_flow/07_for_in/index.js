786c0b700afd7dd5d6c77bbb927a0c268582d0c8
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2021-04-14 19:20:09 +0200

    for in loop examples

--- 04_control_flow/07_for_in/index.js
+++ 04_control_flow/07_for_in/index.js
@@ -1,1 +1,14 @@
-// If...else
+// For...In
+
+const person = {
+    name: 'Gustavo',
+    age: 30
+};
+
+for (let key in person)
+    console.log(key, person[key]);
+
+const colors = ['red', 'green', 'blue'];
+
+for (let index in colors)
+    console.log(index, colors[index]);
