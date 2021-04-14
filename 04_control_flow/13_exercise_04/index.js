0f595b55afac48444232b5019cbe3d33e36bf76d
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2021-04-14 23:46:34 +0200

    refactoring exercise

--- 04_control_flow/13_exercise_04/index.js
+++ 04_control_flow/13_exercise_04/index.js
@@ -1,18 +1,21 @@
 // Speed Limit = 70
 // For every 5 kph over the speed limit the driver got a demerit point
 // if the drivers gets more than 12 points the license is suspended
-checkSpeed(80);
+checkSpeed(75);
 
 function checkSpeed(speed) {
     const speedLimit = 70;
     const suspensionPoints = 12;
     const speedPointsInterval = 5;
 
+    if (speed < speedLimit + speedPointsInterval) {
+        console.log('Ok');
+        return;
+    }
+
     const points = Math.floor((speed - speedLimit) / speedPointsInterval);
 
-    if (points < 1)
-        console.log('Ok');
-    else if (points < suspensionPoints)
+    if (points < suspensionPoints)
         console.log('Points:', points);
     else
         console.log('License suspended');
