951254c00f836c97891a701ecbb571df1afbc53a
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2021-04-15 23:28:46 +0200

    objects section base

---
+++ 05_objects/15_exercise_01/index.js
@@ -0,0 +1,10 @@
+// Write a function that takes two numbers and returns the maximun of the two.
+
+// max takes two numbers a and b and return the maximun of the two
+function max(a, b) {
+    return (a > b) ? a : b;
+}
+
+console.log(max(10, 5));
+console.log(max(10, 25));
+console.log(max(10, 10));
