60210744a82bf08decce669fb300c9b6e6a89fe1
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2021-04-16 15:05:08 +0200

    solving exercise 3

--- 05_objects/17_exercise_03/index.js
+++ 05_objects/17_exercise_03/index.js
@@ -1,10 +1,25 @@
-// Write a function that takes two numbers and returns the maximun of the two.
-
-// max takes two numbers a and b and return the maximun of the two
-function max(a, b) {
-    return (a > b) ? a : b;
+function Address(street, city, zipCode) {
+    this.street = street;
+    this.city = city;
+    this.zipCode = zipCode;
 }
 
-console.log(max(10, 5));
-console.log(max(10, 25));
-console.log(max(10, 10));
+function areEqual(address1, address2) {
+    return address1.street === address2.street &&
+        address1.city === address2.city &&
+        address1.zipCode === address2.zipCode;
+}
+
+function areSame(address1, address2) {
+    return address1 === address2;
+}
+
+const address1 = new Address('a', 'b', 'c');
+const address2 = new Address('a', 'b', 'c');
+const address3 = address1;
+const address4 = new Address('c', 'b', 'a');
+
+console.log('areEqual(address1, address2):', areEqual(address1, address2));
+console.log('areSame(address1, address2):', areSame(address1, address2));
+console.log('areEqual(address1, address4):', areEqual(address1, address4));
+console.log('areSame(address1, address3):', areSame(address1, address3));
