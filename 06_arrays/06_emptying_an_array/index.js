ebe341ecff8c676482fcc6b4fae0e3c8ddfc09b4
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2021-04-18 11:51:27 +0200

    emptying arrays

---
+++ 06_arrays/06_emptying_an_array/index.js
@@ -0,0 +1,10 @@
+const numbers = [1, 2, 3, 4, 5, 6];
+
+console.log(numbers);
+
+numbers.length = 0;
+// numbers.splice(0, numbers.length);
+// while (numbers.length > 0)
+//     numbers.pop();
+
+console.log(numbers);
