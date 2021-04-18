2c9458482c3c8c57cd49d3e9752741d877690e30
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2021-04-18 11:55:53 +0200

    combining and slicing arrays

---
+++ 06_arrays/07_combaning_and_slicing_arrays/index.js
@@ -0,0 +1,12 @@
+const first = [1, 2, 3];
+const second = [4, 5, 6];
+
+// if using reference types it copies the reference
+
+const combined = first.concat(second);
+
+console.log(combined);
+
+const slice = combined.slice(2, 4);
+
+console.log(slice); 
